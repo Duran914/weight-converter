a22bc7a62efac6969f59bc2c8059540d1c9bc2bb
Duran914 <jonathan.duran.100@my.csun.edu>
2018-09-13 23:27:51 -0700

    onChange event expanded

--- convert.js
+++ convert.js
@@ -1,20 +1,25 @@
-// conersions cards
+// JS for weight conversions 
 
-let unitType = document.querySelector('#unit'); // select list item
+let unitType = document.querySelector('#unit'); // <select> list item
 let cards = document.getElementById('output'); // card which displays conversions
-    cards.style.visibility = 'hidden'; // cards hidden before input
+    cards.style.visibility = 'hidden'; // hides cards before input
 
-const weightInput = document.getElementById('weightInput');
+const weightInput = document.getElementById('weightInput'); // input field
 
-// Changes placeholder text on input field
+// Changes placeholder and runs conversions if input field  had a number
+
 unitType.addEventListener('change', () => {
     weightInput.placeholder =  `Enter ${unitType.value}...`
     cards.style.visibility = 'hidden';
-    weightInput.value = ''; // clear input onchange when input has data
-    resetCards(); // set cards to original 
+    resetCards(); // Resets cards to original 
+    if(weightInput.value != ''){
+        computeWeight(); // Runs conversions  
+    }
 });
 
-weightInput.addEventListener('input', (e) => {
+weightInput.addEventListener('input', computeWeight);
+
+function computeWeight(){
     if(weightInput.value != ''){
 
         if(unitType.value === 'pounds'){
@@ -35,9 +40,11 @@ weightInput.addEventListener('input', (e) => {
         cards.style.visibility = 'hidden';
     }
 
-});
+};
 
-function poundsConversion(e){
+// Functions for weight conversions
+
+function poundsConversion(){
     // Do calculations 
     cards.style.visibility = 'visible'; 
       document.getElementById('gramsOutput').innerHTML = `${(weightInput.value / 0.0022046).toFixed(2)} g` ; // Grams
@@ -45,31 +52,31 @@ function poundsConversion(e){
         document.getElementById('ouncesOutput').innerHTML = `${(weightInput.value  * 16).toFixed(2)} oz`; // ounces
     }
 
-function gramsConversion(e){
+function gramsConversion(){
    cards.style.visibility = 'visible'; 
-   console.log(document.getElementById('unit1').innerHTML);
    document.getElementById('unit1').innerHTML = 'Pounds:'; // swap grams for pounds
     document.getElementById('gramsOutput').innerHTML = `${(weightInput.value  * 0.0022046).toFixed(2)} lbs` ; // pounds
      document.getElementById('kiloOutput').innerHTML = `${(weightInput.value  / 1000.0).toFixed(2)} kg`; // Killograms
       document.getElementById('ouncesOutput').innerHTML = `${(weightInput.value  * 0.035274).toFixed(2)}oz`; // ounces
 } 
 
-function killosConversion(e){
+function killosConversion(){
     cards.style.visibility = 'visible'; 
-    console.log(document.getElementById('unit2').innerHTML);
     document.getElementById('gramsOutput').innerHTML = `${(weightInput.value / 0.0010000).toFixed(2)} g` ; // Grams
     document.getElementById('unit2').innerHTML = 'Pounds'; // swap kilos for pounds
      document.getElementById('kiloOutput').innerHTML = `${(weightInput.value *  2.2046).toFixed(2)} lbs`; // pounds
       document.getElementById('ouncesOutput').innerHTML = `${(weightInput.value  * 35.274).toFixed(2)} oz`; // ounces
 } 
 
-function ouncesConversion(e){
+function ouncesConversion(){
     cards.style.visibility = 'visible'; 
     document.getElementById('gramsOutput').innerHTML = `${(weightInput.value / 0.035274).toFixed(2)} g` ; // Grams
      document.getElementById('kiloOutput').innerHTML = `${(weightInput.value / 35.274).toFixed(2)} kg`; // Killograms
      document.getElementById('unit3').innerHTML = 'Pounds'; // swap ounces for pounds
       document.getElementById('ouncesOutput').innerHTML = `${(weightInput.value  * 0.062500).toFixed(2)} lbs`; // pounds
 } 
+
+// Original templete for cards
 
 function resetCards(){
     cards.innerHTML = `
@@ -97,4 +104,4 @@ function resetCards(){
         </h4>
     </div>
 </div>`;
-}
+};
